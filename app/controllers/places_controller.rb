7edaaae8fbf4c3181754660ef87c1f1140046d3b
Ahmed Ahmed <critenks29@gmail.com>
2016-03-06 19:14:34 +0000

    Added login links to navbar

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,7 +1,7 @@
 class PlacesController < ApplicationController
 
   def index
-      @places = Place.all.page(params[:page]).per(5)
+      @places = Place.all.page(params[:page]).per(10)
   end
 
   def new
