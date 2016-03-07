937954c186e021c320f1ba930756393ce79ae1f3
Ahmed Ahmed <critenks29@gmail.com>
2016-03-07 00:17:22 +0000

    Allowing edit places only by the user who created them

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,6 +1,9 @@
 class PlacesController < ApplicationController
 
-    before_action :authenticate_user!, only: [:new, :create]
+
+    before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+    # When a user tries to be sneaky and types in the url
+    # This is to prevent that.
 
   def index
       @places = Place.all#.page(params[:page]).per(10)
@@ -21,10 +24,18 @@ class PlacesController < ApplicationController
 
   def edit
     @place = Place.find(params[:id])
+
+    if @place.user != current_user
+      return render text: 'Not Allowed', status:
+      :forbidden
+    end
   end
 
   def update
     @place = Place.find(params[:id])
+    if @place.user != current_user
+      return render text: 'Not Allowed', status: :forbidden
+    end
     @place.update_attributes(place_params)
     redirect_to root_path
   end
