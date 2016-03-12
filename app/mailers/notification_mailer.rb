d34d185ce581d551d3ba485c58384437c447c818
Ahmed Ahmed <critenks29@gmail.com>
2016-03-12 21:20:02 +0000

    Placed links in my auto mailer

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,9 +2,15 @@ class NotificationMailer < ActionMailer::Base
   default from: "no-reply@nomsterapp.com"
 
 
-  def comment_added
-  	mail(to: "workaahmed16@gmail.com",
-  		 subject: "A comment has been added to your place")
+  def comment_added(comment)
+  	@place = comment.place
+  	@place_owner = @place.user
+  	#I am seeing a pattern when we get info and
+  	#store it in a variable
+  	#we use the '@' operator. Why?
+  	mail(to: @place_owner.email,
+  		 subject: "A comment has been added to #{@place.name}")
+
   end
 
 end
