7371d280e07cae7c6399e278d82269f271497142
Ahmed Ahmed <critenks29@gmail.com>
2016-03-12 19:48:42 +0000

    Added Notification Mailer

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -4,6 +4,7 @@ class PhotosController < ApplicationController
 		@place = Place.find(params[:place_id])
 		@place.photos.create(photo_params)
 		redirect_to place_path(@place)
+	end
 
   	private
 
---
+++ app/mailers/notification_mailer.rb
@@ -0,0 +1,10 @@
+class NotificationMailer < ActionMailer::Base
+  default from: "no-reply@nomsterapp.com"
+
+
+  def comment_added
+  	mail(to: "workaahmed16@gmail.com",
+  		 subject: "A comment has been added to your place")
+  end
+
+end
