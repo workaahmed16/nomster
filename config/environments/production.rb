2280760fed812589e8b7d5831ba7819264816f2c
Ahmed Ahmed <critenks29@gmail.com>
2016-03-13 18:05:36 +0000

    Added mailgun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,17 @@
 Nomster::Application.configure do
+
+
+    ActionMailer::Base.smtp_settings = {
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => 'nomster-ahmed-ahmed.herokuapp.com',
+    :authentication => :plain,
+  }
+  ActionMailer::Base.delivery_method = :smtp
+
+
   config.action_mailer.default_url_options = { host: 'nomster-ahmed-ahmed.herokuapp.com' }
   # Settings specified here will take precedence over those in config/application.rb.
 
