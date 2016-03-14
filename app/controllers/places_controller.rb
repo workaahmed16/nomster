5485a6ddd5e3f9c163cd3a72e860b19073deb9c3
Ahmed Ahmed <critenks29@gmail.com>
2016-03-14 22:51:15 +0000

    Place create page redirects to place created

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -16,7 +16,7 @@ class PlacesController < ApplicationController
   def create
   @place = current_user.places.create(place_params)
       if @place.valid?
-        redirect_to root_path
+        redirect_to place_path(@place)
       else
         render :new, status: :unprocessable_entity
       end
@@ -64,7 +64,7 @@ class PlacesController < ApplicationController
   private
 
   def place_params
-    params.require(:place).permit(:name, :description, :address)
+    params.require(:place).permit(:name, :description, :address,)
   end
   
 end
